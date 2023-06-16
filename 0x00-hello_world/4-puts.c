a1ff742fc70028720078218afde67988a1990581
ThandowMbatha <mthiyatw@gmail.com>
2023-06-15 21:51:59 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 
 /**
  * main - Entry point of the program
- *Return: 0 (success)
+ *
+ * Return: 0 (success)
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\");
-	return (0);
+    puts("\"Programming is like building a multilingual puzzle.\"");
+    return (0);
 }
