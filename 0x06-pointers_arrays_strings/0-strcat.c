59ae530c6afc1910601571a294923bacadfa2e70
ThandowMbatha <mthiyatw@gmail.com>
2023-06-28 12:53:38 -0700

    Task 1-7

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - it concatenates two strings
+ * @dest: input value
+ * @src: input value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int p;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	p = 0;
+	while (src[p] != '\0')
+	{
+		dest[i] = src[p];
+		i++;
+		p++;
+	}
+
+	dest[i] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - it encodes into 1337speak
+ * @n: input value
+ * Return: n value
+ */
+char *leet(char *n)
+{
+	int i, p;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	for (i = 0; n[i] != '\0'; i++)
+	{
+		for (p = 0; p < 10; p++)
+		{
+			if (n[i] == s1[p])
+			{
+				n[i] = s2[p];
+			}
+		}
+	}
+	return (n);
+}
